c9067165f64c9add926664f351749195085e97c3
Mauricio Vilela <tqi_mvilela@uolinc.com>
2023-02-11 17:46:46 -0300

    subindo projeto

---
+++ module-01-basic/src/main/java/com/tqi/course/controller/LoggingController.java
@@ -0,0 +1,23 @@
+package com.tqi.course.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/log")
+public class LoggingController {
+
+    private Log log = LogFactory.getLog(LoggingController.class);
+
+    @GetMapping
+    public String buildLog() {
+        log.info("This is an INFO level message");
+        log.warn("This is a WARN level message");
+        log.error("This is an ERROR level message");
+        return "See the log for details";
+    }
+
+}
